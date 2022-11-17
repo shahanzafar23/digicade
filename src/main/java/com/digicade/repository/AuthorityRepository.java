51a4b93a1894cce50e3745e91d8e983a1ab0c909
waqar ahmad <engrwaqar37@gmail.com>
2022-11-17 14:45:17 +0500

    Initial version of digicade generated by generator-jhipster@7.9.3

---
+++ src/main/java/com/digicade/repository/AuthorityRepository.java
@@ -0,0 +1,9 @@
+package com.digicade.repository;
+
+import com.digicade.domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the {@link Authority} entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {}
